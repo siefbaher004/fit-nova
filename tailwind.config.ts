8400c6d55987cf82bb66a965313f339350adc549
Sief Eldinq <seifbaher15@gmail.com>
2025-10-24 04:35:24 +0300

    feat(theme): add custom colors theming setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,8 +14,14 @@ const config: Config = {
       },
 
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: "var(--color-bg-main)",
+        foreground: "var(--color-primary-main)",
+        // Chat
+        chat: "var(--chat-bg)",
+        chatHide: "var(--chat-user-bg-hide)",
+        chatUser: "var(--chat-user-bg)",
+        chatBot: "var(--chat-bot-bg)",
+        prev: "var(--chat-bg-previous-conversations)",
       },
     },
   },
